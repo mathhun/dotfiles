c1fef02e57b30f72b50f8f0bc2868209c0720da6
mathhun <achemer@gmail.com>
2015-02-06 01:00:15 +0900

    Add python mode config etc.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,6 +8,20 @@
 ;; no beep
 (setq visible-bell t)
 (setq ring-bell-function 'ignore)
+;; no backup
+(setq make-backup-files nil)
+(setq aut-save-default nil)
+;; tab
+(setq-default tab-width 4
+          indent-tabs-mode nil)
+;; path
+(setq exec-path (cons (concat (getenv "HOME") "/.pyenv/shims") exec-path))
+
+;; frame size
+(add-to-list 'default-frame-alist '(left . -1))
+(add-to-list 'default-frame-alist '(top . 0))
+(add-to-list 'default-frame-alist '(height . 60))
+(add-to-list 'default-frame-alist '(width . 130))
 
 ;; key binding
 (global-set-key "\C-h" 'delete-backward-char)
@@ -32,9 +46,56 @@
       scroll-margin 0
       scroll-step 1)
 
+;; dialog
+(defadvice yes-or-no-p (around prevent-dialog activate)
+  "Prevent yes-or-no-p from activating a dialog"
+  (let ((use-dialog-box nil))
+    ad-do-it))
+(defadvice y-or-n-p (around prevent-dialog-yorn activate)
+  "Prevent y-or-n-p from activating a dialog"
+  (let ((use-dialog-box nil))
+    ad-do-it))
+
 ;; cask
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
+
+;;
+;; programming
+;;
+
+(yas-global-mode t)
+(require 'auto-complete-config)
+(ac-config-default)
+
+;; flymake
+(setq flymake-gui-warnings-enabled nil)
+
+;; python
+(add-hook 'python-mode-hook
+      (lambda ()
+        (define-key python-mode-map (kbd "\C-m") 'newline-and-indent)
+        (define-key python-mode-map (kbd "RET") 'newline-and-indent)))
+
+(add-hook 'before-save-hook 'py-autopep8-before-save)
+
+(require 'tramp-cmds)
+(when (load "flymake" t)
+  (defun flymake-pyflakes-init ()
+    ;; Make sure it's not a remote buffer or flymake would not work
+    (when (not (subsetp (list (current-buffer)) (tramp-list-remote-buffers)))
+      (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                         'flymake-create-temp-inplace))
+             (local-file (file-relative-name
+                          temp-file
+                          (file-name-directory buffer-file-name))))
+        (list "pyflakes" (list local-file)))))
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pyflakes-init)))
+
+(add-hook 'python-mode-hook
+          (lambda ()
+            (flymake-mode t)))
 
 ;; dired
 (put 'dired-find-alternate-file 'disabled nil)
