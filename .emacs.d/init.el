fc00f464147f5fac86a0b27952fd9bdd09f08bbd
mathhun <achemer@gmail.com>
2015-04-29 09:01:06 +0900

    web mode config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -296,10 +296,15 @@
 ;;
 
 (require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
+;; indent
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-css-indent-offset 2)
 (setq web-mode-code-indent-offset 2)
-(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
+;; padding
+(setq web-mode-style-padding 2)
+(setq web-mode-script-padding 2)
+
 
 ;;
 ;; React / JSX
